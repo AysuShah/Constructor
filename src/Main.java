8c6443b424d33abcbdc89b3bcce1b8497983098e
aysu.shahmirzayeva <aysu1515@gmail.com>
2023-04-12 11:15:28 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age;
+        String name;
+        double gpa;
+
+        Student student1 = new Student(15, 3.0, "Aysu");
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+    public Student(int age, double gpa, String name){
+
+        
+
+    }
+
+    public Student() {
+    }
+}
